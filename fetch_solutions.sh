131e068fac874c3b9429c8415740a0660a0683f0
Dan Clark <danclark@redhat.com>
2021-12-27 10:08:29 -0500

    Redirect output to access git repo

--- fetch_solutions.sh
+++ fetch_solutions.sh
@@ -3,23 +3,25 @@
 for i in $(seq 5000000 5298651)
 do
 
-  if test -e "./access.redhat.com/solutions/${i}" || grep -q "${i}" dne.txt
+  echo "Checking solutions article: ${i}"
+
+  if test -e "../access.redhat.com/solutions/${i}" || grep -q "${i}" dne.txt
   then
     continue
   fi
 
   echo "Processing solutions article: ${i}"
 
-  node fetch_solutions.js "${i}" > "./access.redhat.com/solutions/${i}"
+  node fetch_solutions.js "${i}" > "../access.redhat.com/solutions/${i}"
 
   # If the article doesn't exist or returns empty, remove it
-  if ! test -s "./access.redhat.com/solutions/${i}"  || grep -q 'The page you are looking for is not here' "./access.redhat.com/solutions/${i}"
+  if ! test -s "../access.redhat.com/solutions/${i}"  || grep -q 'The page you are looking for is not here' "../access.redhat.com/solutions/${i}"
   then
     if ! grep -q "${i}" dne.txt
     then
       echo "${i}" >> dne.txt
     fi
-    rm -fv "./access.redhat.com/solutions/${i}"
+    rm -fv "../access.redhat.com/solutions/${i}"
   fi
 
 done
