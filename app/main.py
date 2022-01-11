76fadecfb11a36d704620959d06a6344b74b9b3d
Dan Clark <danclark@redhat.com>
2022-01-10 20:38:07 -0500

    Updates to serve cves, errata, and solutions articles

--- app/main.py
+++ app/main.py
@@ -1,11 +1,111 @@
+import os
 from starlette.applications import Starlette
 from starlette.routing import Mount
 from starlette.staticfiles import StaticFiles
+from starlette.requests import Request
+from starlette.responses import Response
+from starlette.responses import HTMLResponse
+from starlette.templating import Jinja2Templates
+from starlette.responses import JSONResponse
+import uvicorn
+import json
+
+#with open("./hydra/rest/securitydata/cve.json", "r") as read_file:
+#    data = json.load(read_file)
+
+#async def cve(request)
+#  return PlainTextResponse("In CVE section")
+
+templates = Jinja2Templates(directory='templates')
+
+app = Starlette(debug=True)
+#app.mount('/static', StaticFiles(directory='statics'), name='static')
+#app.mount('/solutions', StaticFiles(directory='access.redhat.com/solutions/'), name='solutions')
+
+#@app.route('/')
+#async def homepage(request):
+#    template = "index.html"
+#    context = {"request": request}
+#    return templates.TemplateResponse(template, context)
+
+@app.route("/solutions/{id}")
+async def example(request):
+    content = '%s %s' % (request.method, request.url.path)
+    content = os.path.basename(os.path.normpath(content))
+
+    filename = './access.redhat.com/solutions/' + content
+
+    if not os.path.isfile(filename):
+        return Response(status_code=404)
+
+    with open(filename) as f:
+        content = f.read()
+
+    #content_type, _ = guess_type(filename)
+    return Response(content, media_type='html')
+
+@app.route("/errata/{id}")
+async def errata(request):
+    content = '%s %s' % (request.method, request.url.path)
+    content = os.path.basename(os.path.normpath(content))
+
+    filename = './access.redhat.com/errata/' + content
+
+    if not os.path.isfile(filename):
+        return Response(status_code=404)
+
+    with open(filename) as f:
+        content = f.read()
+
+    #content_type, _ = guess_type(filename)
+    return Response(content, media_type='html')
+
+#@app.route("/security/cve/{id}")
+@app.route("hydra/rest/securitydata/cvrf/{id}.json")
+async def cvrf(request):
+    qp = request.query_params
+
+    print(str(qp))
+    # > foo=bar
+
+    return JSONResponse({k: v for (k, v) in qp.items()})
+
+@app.route("hydra/rest/securitydata/cve/{id}.json")
+async def cve(request):
+    qp = request.query_params
+
+    print(str(qp))
+    # > foo=bar
+
+    return JSONResponse({k: v for (k, v) in qp.items()})
+
+@app.route('/error')
+async def error(request):
+    """
+    An example error. Switch the `debug` setting to see either tracebacks or 500 pages.
+    """
+    raise RuntimeError("Oh no")
 
 
-routes = [
-    Mount('/hydra/rest/securitydata/cve', app=StaticFiles(directory='./hydra/rest/securitydata/cve/'), name="cve"),
-    Mount('/hydra/rest/securitydata/cvrf', app=StaticFiles(directory='./hydra/rest/securitydata/cvrf/'), name="cvrf")
-]
+@app.exception_handler(404)
+async def not_found(request, exc):
+    """
+    Return an HTTP 404 page.
+    """
+    template = "404.html"
+    context = {"request": request}
+    return templates.TemplateResponse(template, context, status_code=404)
 
-app = Starlette(routes=routes)
+
+@app.exception_handler(500)
+async def server_error(request, exc):
+    """
+    Return an HTTP 500 page.
+    """
+    template = "500.html"
+    context = {"request": request}
+    return templates.TemplateResponse(template, context, status_code=500)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='0.0.0.0', port=8000)
