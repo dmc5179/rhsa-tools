16f1429d200b71495c3bf18b926d6f4148ee2db0
Dan Clark <danclark@redhat.com>
2022-01-17 00:32:30 -0500

    Update app with jq subprocess calls

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 from starlette.applications import Starlette
 from starlette.routing import Mount
 from starlette.staticfiles import StaticFiles
@@ -9,10 +10,13 @@ from starlette.templating import Jinja2Templates
 from starlette.responses import JSONResponse
 import uvicorn
 import json
+import pyjq
 
-f = open("./hydra/rest/securitydata/cve.json", "r")
-cve_json = json.load(f)
-f.close()
+#f = open("./data/cve.json", "r")
+#cve_json = json.load(f)
+#f.close()
+with open('./data/cve_jq.json') as f:
+    cve_json = [json.loads(line) for line in f]
 
 templates = Jinja2Templates(directory='templates')
 
@@ -81,36 +85,59 @@ async def cvrf(request):
 
     return JSONResponse({k: v for (k, v) in qp.items()})
 
+async def select_before(before):
+  print("Selecting before: " + before)
+  return 'map(. | select(.public_date |. != null and . != "")) | map(select(.public_date | . <= $e + "z")) | select(length>0)'
+
+async def select_after(after):
+  print("Selecting after: " + after)
+
+async def select_ids(ids):
+  return ".[] | select(.CVE == \"" + ids + "\")"
+
 @app.route("/hydra/rest/securitydata/cve.json")
 async def cve_search(request):
-    before=""
-    after=""
-    ids=""
-    bug=""
-    advisory=""
-    severity="low"
-    package=""
-    product=""
-    cwe=""
-    cvss_score=""
-    cvss3_score=""
+    qp = request.query_params
+    jq_qry = ""
+    jq_args = ""
+
+    for key in qp.keys():
+      if key == "before":
+        jq_qry += await select_before(qp['before'])
+        jq_args += '--arg e ' + qp['before']
+      if key == "after":
+        jq_qry += await select_after(qp['after'])
+      if key == "ids":
+        jq_qry += await select_ids(qp['ids'])
+      if key == "bug":
+        jq_qry += await select_(qp[''])
+      if key == "advisory":
+        jq_qry += await select_(qp[''])
+      if key == "severity":
+        jq_qry += await select_(qp[''])
+      if key == "package":
+        jq_qry += await select_(qp[''])
+      if key == "product":
+        jq_qry += await select_(qp[''])
+      if key == "cwe":
+        jq_qry += await select_(qp[''])
+      if key == "cvss_score":
+        jq_qry += await select_(qp[''])
+      if key == "cvss3_score":
+        jq_qry += await select_(qp[''])
+      if key == "created_days_ago":
+        jq_qry += await select_(qp[''])
+
+# execute query
+    jq_cmd = "jq -c -r '" + jq_qry + "'" + " ./data/cve_jq.json"
+    results = subprocess.run(jq_cmd, shell=True, check=True, capture_output=True, text=True)
+    response = json.loads(results.stdout)
+
+# pageniate if needed
     page=1
     per_page=1000
-    created_days_ago=""
 
-    results=[]
-
-    for key in cve_json:
-      if severity and key['severity'] != severity:
-        continue
-      results.append(key);
-
-    #qp = request.query_params
-    #print(str(qp))
-    ## > foo=bar
-
-    #return JSONResponse({k: v for (k, v) in qp.items()})
-    return JSONResponse(results)
+    return JSONResponse(response)
 
 @app.route("/hydra/rest/securitydata/cve/{id}.json")
 async def cve(request):
