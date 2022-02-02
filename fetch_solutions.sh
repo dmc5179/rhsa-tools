0587202523a2d7c4f09cfa1801af619fbf083b20
Dan Clark <danclark@redhat.com>
2022-02-01 22:24:06 -0500

    Update to support different state files

--- fetch_solutions.sh
+++ fetch_solutions.sh
@@ -1,11 +1,15 @@
 #!/bin/bash -e
 
-for i in $(seq 5000000 5298651)
+STATE="dne.txt"
+
+START=$(tail -n 1 ${STATE})
+
+for i in $(seq ${START} 5999999)
 do
 
   echo "Checking solutions article: ${i}"
 
-  if test -e "../access.redhat.com/solutions/${i}" || grep -q "${i}" dne.txt
+  if test -e "../access.redhat.com/solutions/${i}" || grep -q "${i}" "${STATE}"
   then
     continue
   fi
@@ -17,9 +21,9 @@ do
   # If the article doesn't exist or returns empty, remove it
   if ! test -s "../access.redhat.com/solutions/${i}"  || grep -q 'The page you are looking for is not here' "../access.redhat.com/solutions/${i}"
   then
-    if ! grep -q "${i}" dne.txt
+    if ! grep -q "${i}" "${STATE}"
     then
-      echo "${i}" >> dne.txt
+      echo "${i}" >> "${STATE}"
     fi
     rm -fv "../access.redhat.com/solutions/${i}"
   fi
